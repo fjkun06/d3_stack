2ad89a3a9781316b351d788c6f329fcbb8de048d
= <=>
2023-04-23 16:22:26 +0100

    Completed loader

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector("body main").style.overflow = "hidden";
+// document.querySelector("body main").style.overflow = "hidden";
 const loader = document.getElementById("loader");
 let timer;
 let count = 0;
@@ -10,7 +10,7 @@ const cancel = (timestamp) => {
   // console.log(elapsed);
   //do this after 5s
   if (elapsed > 5000) {
-    document.querySelector("body main").style.overflow = "";
+    // document.querySelector("body main").style.overflow = "";
     loader.style.display = "none";
     cancelAnimationFrame(timer);
   }
