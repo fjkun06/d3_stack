aba84ac9d164ae4359627e851c6c3aac2e268d2c
= <=>
2023-05-02 09:39:02 +0100

    responsive axes

--- scripts2/data2.js
+++ scripts2/data2.js
@@ -7,14 +7,15 @@ const bigSet = {
 //function to group data by category
 /*************************Drawing Axes and Configuring Scale********************* */
 
-const width = 800;
+const width = 700;
 const height = 400;
 const margin = 50;
 const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("height", height + 100)
-  .attr("width", "100vw");
+  .select("svg")
+  .style('border','1px solid red')
+  // .append("svg")
+  // .attr("height", height + 100)
+  // .attr("width", "100vw");
 // .attr("width", width + 100);
 const scaleX = d3
   .scaleLinear()
@@ -23,15 +24,17 @@ const scaleX = d3
 const scaleY = d3
   .scaleLinear()
   .domain([0, height])
-  .range([height - margin, 0])
+  // .range([200, 0])
+  .range([height, 0])
   .nice();
+  console.log(scaleY(height-380));
 const axes = svg.append("g").attr("class", "axes").attr("transform", `translate(${margin},${margin})`);
 
 const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 /*************************Adding legends********************* */
-svg.append("g").append("text").text("Number Of Students").attr("fill", "black").attr("transform", "translate(30,150) rotate(90)").attr("class", "legend");
-svg.append("g").append("text").text("Subjects").attr("fill", "black").attr("transform", "translate(400,450)").attr("class", "legend");
+svg.append("g").append("text").text("Number Of Students").attr("fill", "black").attr("transform", "translate(0,170) rotate(90)").attr("class", "legend");
+svg.append("g").append("text").text("Subjects").attr("fill", "black").attr("transform", "translate(320,500)").attr("class", "legend");
 svg.append("g").attr("class", "map");
 svg.append("g").attr("class", "title");
 //adding title
@@ -81,7 +84,10 @@ const init = () => {
     .tickFormat(function (d, i) {
       return subjectAbbreviations[i]; //"Year1 Year2, etc depending on the tick value - 0,1,2,3,4"
     });
-  const yAxis = d3.axisLeft(scaleY).tickSize(50).tickSizeOuter(0);
+  const yAxis = d3.axisLeft(scaleY)
+  .tickSize(5)
+  .tickSizeOuter(0)
+  // .ticks(14);
 
   // .tickSize(width);
 
@@ -89,62 +95,65 @@ const init = () => {
     .append("g")
     .attr("class", "x-axis")
     .call(xAxis)
-    .attr("transform", `translate(${[0, height - margin]})`);
+    .attr("transform", `translate(${[-50, height]})`);
   axes
     .append("g")
     .attr("class", "y-axis")
 
     .call(yAxis)
-    .attr("transform", `translate(${[margin, 0]})`);
+    // .attr("transform", `translate(${[margin, 0]},0)`);
   svg.append("g").attr("class", "draw");
 
   //Styling domains n ticks
-  d3.selectAll(".domain").each(function () {
-    d3.select(this).attr("opacity", 0);
-  });
-  const yAxisTicks = d3.selectAll(".y-axis .tick").each(function (d, i) {
-    d3.select(this)
-      .select("line")
-      .attr("x1", `-10`)
-      .attr("x2", width - 80);
-    d3.select(this).select("text").attr("x", `-20`);
-  });
-  const xAxisTicks = d3.selectAll(".x-axis .tick").each(function (d, i) {
-    d3.select(this)
-      .select("line")
-      .attr("opacity", 0);
-    d3.select(this).select("text").attr("y", `10`);
-  });
-  console.log(yAxisTicks);
+  // d3.selectAll(".domain").each(function () {
+  //   d3.select(this).attr("opacity", 0);
+  // });
+  // const yAxisTicks = d3.selectAll(".y-axis .tick").each(function (d, i) {
+  //   d3.select(this)
+  //     .select("line")
+  //     .attr("x1", `-10`)
+  //     .style("stroke-width", `.25`)
+  //     .attr("x2", width - 80);
+  //   d3.select(this).select("text").attr("x", `-20`);
+  // });
+  // const xAxisTicks = d3.selectAll(".x-axis .tick").each(function (d, i) {
+  //   d3.select(this)
+  //     .select("line")
+  //     .attr("opacity", 0);
+  //   d3.select(this).select("text")
+  //   .attr("y", `10`)
+  //   .attr("x", `-50`);
+  // });
+  // console.log(yAxisTicks);
 
   //drawing chart legend
-  const map = d3
-    .select("g.map")
-    .attr("transform", () => `translate(880,60)`)
-    .selectAll("g")
-    .data(subjectWithAbbreviation)
-    .join("g")
-    .transition()
-    .each(function (d, i) {
-      d3.select(this)
-        .datum(d)
-        .append("rect")
-        .attr("width", 20)
-        .attr("height", 10)
-        .attr("fill", colorScale(i))
-        .attr("y", i * 20)
-        .attr("transform", " translate(0, -9)")
-        .attr("x", -30);
+  // const map = d3
+  //   .select("g.map")
+  //   .attr("transform", () => `translate(880,60)`)
+  //   .selectAll("g")
+  //   .data(subjectWithAbbreviation)
+  //   .join("g")
+  //   .transition()
+  //   .each(function (d, i) {
+  //     d3.select(this)
+  //       .datum(d)
+  //       .append("rect")
+  //       .attr("width", 20)
+  //       .attr("height", 10)
+  //       .attr("fill", colorScale(i))
+  //       .attr("y", i * 20)
+  //       .attr("transform", " translate(0, -9)")
+  //       .attr("x", -30);
 
-      d3.select(this)
-        .datum(d)
-        .append("text")
-        .attr("class", "maptext")
-        .attr("y", i * 20)
-        .text((d) => d);
-    })
-    .attr("width", "50")
-    .attr("height", "50");
+  //     d3.select(this)
+  //       .datum(d)
+  //       .append("text")
+  //       .attr("class", "maptext")
+  //       .attr("y", i * 20)
+  //       .text((d) => d);
+  //   })
+  //   .attr("width", "50")
+  //   .attr("height", "50");
 };
 
 // init()
@@ -156,14 +165,14 @@ const test = (year) => {
   // .sort((a, b) => d3.ascending(a, b));
   const subjectAbbreviations = bigSet.subjects.map((data) => `${data.slice(0, 3).toUpperCase()}`);
   console.log(subjectAbbreviations);
-  draw(datum, year);
+  // draw(datum, year);
 };
 
 const draw = (datum, abbrev) => {
   console.log(datum);
   // drawing bars
   const g = d3.select("g.draw");
-  g.style("border", "1px solid red")
+  g
     .selectAll("rect")
     .data(datum)
     .join("rect")
@@ -175,7 +184,7 @@ const draw = (datum, abbrev) => {
       return scaleY(400 - d);
     })
     // .attr("height", (d) => scaleY(350 - d))
-    .attr("transform", (k, i) => `translate(${[scaleX(i) + 45 + margin, scaleY(0) + margin]}) scale(1,-1)`)
+    .attr("transform", (k, i) => `translate(${[scaleX(i) - 5 + margin, scaleY(0) + margin]}) scale(1,-1)`)
     // .attr("transform", (k) => `translate(${[scaleX(k[0]), scaleY(k[1])]})`)
     .attr("fill", (k, i) => colorScale(i));
 
@@ -187,7 +196,7 @@ const draw = (datum, abbrev) => {
     .transition()
     .duration(1000)
     .attr("y", (d) => scaleY(d) + 70)
-    .attr("x", (d, i) => scaleX(i) + 55 + margin)
+    .attr("x", (d, i) => scaleX(i) + 60)
     .style("fill", "white");
 
   //adding title
