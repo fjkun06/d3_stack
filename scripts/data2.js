20cd2b64c3c01b0fb93f1cd71af951d3bf1e80ec
= <=>
2023-05-03 16:24:43 +0100

    ...

--- scripts/data2.js
+++ scripts/data2.js
@@ -11,7 +11,8 @@ const width = 700;
 // const width = 1100;
 const height = 400;
 const margin = 50;
-const svg = d3.select("svg#s1").style("border", "1px solid red");
+const svg = d3.select("svg#s1")
+// .style("border", "1px solid red");
 // .style("transform", "translate(0px, 40px)");
 const scaleX = d3
   .scaleLinear()
@@ -26,13 +27,28 @@ const axes = svg.append("g").attr("class", "axes").attr("transform", `translate(
 
 const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 /*************************Adding legends********************* */
-svg.append("g").append("text").text("Number Of Students").attr("fill", "black").attr("transform", "translate(10,170) rotate(90)").attr("class", "legend");
-svg.append("g").append("text").text("Subjects").attr("fill", "black").attr("transform", "translate(320,500)").attr("class", "legend");
+svg.append("g").append("text").text("Number Of Students").attr("fill", "black").style("font-weight", "bold").attr("transform", "translate(10,170) rotate(90)").attr("class", "legend");
+svg.append("g").append("text").text("Subjects").attr("fill", "black").style("font-weight", "bold").attr("transform", "translate(320,493)").attr("class", "legend");
 svg.append("g").attr("class", "map");
 svg.append("g").attr("class", "title");
 //adding title
-d3.select("g.title").append("text").text(`The Distribution of Enrollment across The Top 10 Majors at Stanford University`).attr("fill", "black").attr("x", 120).attr("y", 10);
-d3.select("g.title").append("text").attr("class", "titletext").text(` for The 2011-12 Academic Year`).attr("fill", "black").attr("x", 280).attr("y", 25);
+d3.select("g.title")
+  .append("text")
+  .text(`Top 10 Majors at Stanford University Enrollment Distribution`)
+  .attr("fill", "black")
+  .style("font-weight", "bold")
+  .style("font-family", "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif")
+  .attr("x", 170)
+  .attr("y", 20);
+d3.select("g.title")
+  .append("text")
+  .attr("class", "titletext")
+  .text(` for The 2011-12 Academic Year`)
+  .attr("fill", "black")
+  .style("font-family", "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif")
+  .style("font-weight", "bold")
+  .attr("x", 280)
+  .attr("y", 40);
 
 /*************************Data Fetching********************* */
 
@@ -54,10 +70,10 @@ d3.csv("../StanfordTopTenMajors2010s.csv", (bunch) => {
   bigSet.years = [...bigSet.years];
   bigSet.subjects = [...bigSet.subjects];
   bigSet.scores = [...bigSet.scores];
+
   bigSet2.detailedSubjects = [...bigSet.subjects].map((subj) => Object.assign({}, { name: subj, abbrev: subj.slice(0, 3).toUpperCase() }));
 
   //displaying year options
-  feedSubjects(bigSet2.detailedSubjects);
   feedYears(bigSet.years);
   init();
   test("2011-12");
@@ -65,6 +81,11 @@ d3.csv("../StanfordTopTenMajors2010s.csv", (bunch) => {
 
 /*************************Drawing Axes********************* */
 const init = () => {
+  const datum = bigSet.data.filter((el) => el.year === '2011-12').sort((a, b) => d3.ascending(a.numberOfStudents, b.numberOfStudents)).map((el) => el.subject);
+  const detailedSubjects = [...datum].map((subj) => `${subj} (${subj.slice(0, 3).toUpperCase()})`);
+  console.log(detailedSubjects);
+  feedSubjects(detailedSubjects);
+
   const subjectWithAbbreviation = bigSet.subjects.map((data) => `${data} (${data.slice(0, 3).toUpperCase()})`);
   const subjectAbbreviations = ["", ...bigSet.subjects.map((data) => `${data.slice(0, 3).toUpperCase()}`)];
   const xAxis = d3
@@ -104,50 +125,15 @@ const init = () => {
   });
 
   // drawing chart legend
-  const map = d3
-    .select("g.map")
-    .attr("transform", () => `translate(880,60)`)
-    .selectAll("g")
-    .data(subjectWithAbbreviation)
-    .join("g")
-    .transition()
-    .each(function (d, i) {
-      d3.select(this)
-        .datum(d)
-        .append("rect")
-        .attr("width", 20)
-        .attr("height", 10)
-        .attr("fill", colorScale(i))
-        .attr("y", i * 20)
-        .attr("transform", " translate(0, -9)")
-        .attr("x", -30);
-
-      d3.select(this)
-        .datum(d)
-        .append("text")
-        .attr("class", "maptext")
-        .attr("y", i * 20)
-        .text((d) => d);
-    })
-    .attr("width", "50")
-    .attr("height", "50");
   //adding legend to labels
-  console.log(d3.select("#subject").selectAll("label"));
-  d3.select("#subject")
-    .selectAll("label")
-    .each(function (_,i) {
-      // [...d3.select(this)._groups[0]][0]
-      d3.select(this)
-        .insert("span")
-        .attr("class", 'legspan')
-        // .attr("width", 20)
-        // .attr("height", 10)
-        .style("background-color", colorScale(i))
-        // .attr("y", i * 20)
-        // .attr("transform", " translate(0, -9)")
-        // .attr("x", -30);
-      //  [...d3.select(this)._groups[0]][0].innerHTML
-      console.log([...d3.select(this)._groups[0]][0].innerHTML);
+  // console.log(d3.select("#subject").selectAll("label"));
+  d3.select("#subject").datum(detailedSubjects)
+    .selectAll("label").data(d=>d)
+    .each(function (datum, i) {
+      d3.select(this).text(d => d)
+      d3.select(this).insert("span").attr("class", "legspan").style("background-color", colorScale(i));
+      // console.log([...d3.select(this)._groups[0]][0].textContent);
+      // console.log(datum);
     });
 };
 
@@ -167,7 +153,10 @@ const test = (year) => {
 draw = (datum, abbrev) => {
   const scores = datum.map((x) => x.numberOfStudents);
   const subjectAbbreviations = ["", ...datum.map((x) => x.subject).map((data) => `${data.slice(0, 3).toUpperCase()}`)];
+  const subjects = ["", ...datum.map((x) => x.subject)]
+  const detailedSubjects = [...datum.map((x) => x.subject)].map((subj) => `${subj} (${subj.slice(0, 3).toUpperCase()})`);
 
+  console.log('sortedsubjects: ',subjects);
   //sorting subjects axis
   const xAxis = d3
     .axisBottom(scaleX)
@@ -199,4 +188,16 @@ draw = (datum, abbrev) => {
 
   //adding title
   d3.select("text.titletext").text(` for The ${abbrev} Academic Year`);
+
+  //updating subjects legend
+  feedSubjects(detailedSubjects);
+
+  // d3.select("#subject").datum(abbrev)
+  // .selectAll("label").data(d=>d)
+  // .each(function (datum, i) {
+  //   d3.select(this).text(d => d)
+  //   d3.select(this).insert("span").attr("class", "legspan").style("background-color", colorScale(i));
+  //   // console.log([...d3.select(this)._groups[0]][0].textContent);
+  //   // console.log(datum);
+  // });
 };
